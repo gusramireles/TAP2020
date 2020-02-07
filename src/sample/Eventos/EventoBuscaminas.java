e1d7926ee46e5b879aae077f730146bc2962ddd3
gusramireles <18031000@itcelaya.edu.mx>
2020-02-06 18:36:29 -0600

    Topicos2020

--- src/sample/Eventos/EventoBuscaminas.java
+++ src/sample/Eventos/EventoBuscaminas.java
@@ -5,12 +5,19 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 public class EventoBuscaminas implements EventHandler {
 
     private TextField _txtNoRows, _txtNoCols;
     private GridPane _gdpCampo;
     private Button[][] _arrBtnCeldas;
+    private VBox vbox;
+
+    public EventoBuscaminas(TextField _txtNR, TextField _txtNC){
+        this._txtNoRows = _txtNR;
+        this._txtNoCols = _txtNC;
+    }
 
     @Override
     public void handle(Event event) {
